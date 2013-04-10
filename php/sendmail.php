37d453f62a114ff1537a455f34b605ad7ebec866
carrasker27 <carras_7@live.com.mx>
2013-04-10 00:21:29 -0500

    punto de partida de la publicacion de la pagina

--- php/sendmail.php
+++ php/sendmail.php
@@ -8,50 +8,22 @@
 
 
     //Recuperar los datos que serviran para enviar el correo
-     $seEnvio;      //Para determinar si se envio o no el correo
-     $destinatario = 'hcarrasco@zoetux.com';        //A quien se envia
+     $destinatario = 'hcarrasco@zoetux.com,info@zoetux.com';        //A quien se envia
      $elmensaje = str_replace("\n.", "\n..", $mensaje);     //por si el mensaje empieza con un punto ponerle 2
      $elmensaje = wordwrap($elmensaje, 70);                       //dividir el mensaje en trozos de 70 cols
      //Recupear el asunto
      $asunto = "Informacion " . $empresa;
-     //Formatear un poco el texto que escribio el usuario (asunto) en la caja
-     //de comentario con ayuda de HTML
-	$cuerpomsg ='
-		<html>
-			<head>
-				<title>Tienes un mensaje!!</title>
-			</head>
-			<body>
-				<p>Holaa Gonza, alguien te mando un mensaje de tu pagina web http://gonzaslive.aegislinux.com.mx</p>
-				<table>
-					<tr>
-						<td><b>Tu mensaje es:</b><br></td>
-					</tr>
-					<tr>
-						<td>'.$elmensaje.'</td>
-					</tr>
-					<tr>
-						<td><br><a href="http://gonzaslive.aegislinux.com.mx/ejemplos/enviar_correo/responder.php?correo='.$_POST['elcorreo'].'&asunto=FW: '.$asunto.'&nombre='.$_POST['nombr'].'">Responder</a></td>
-					</tr>
-				</table>
-			</body>
-		</html>
-		';
-//Establecer cabeceras para la funcion mail()
+     $cuerpomsg =$elmensaje;
+	//Establecer cabeceras para la funcion mail()
     //version MIME
     $cabeceras = "MIME-Version: 1.0\r\n";
-    //Tipo de info
-    $cabeceras .= "Content-type: text/html; charset=iso-8859-1\r\n";
     //direccion del remitente
     $cabeceras .= "From: ".$name." <".$mail.">";
-    if(mail($destinatario,$asunto,$cuerpomsg,$cabeceras))
-        $seEnvio = true;
-    else
-        $seEnvio = false;
- 
-//Enviar el estado del envio (por metodo GET ) y redirigir navegador al archivo index.php
-        if($seEnvio == true)
-    {
-        header('Location: ../index_new.html');
+    if(mail($destinatario,$asunto,$cuerpomsg,$cabeceras)){
+        header('Location: ../index.html');
+    }
+    else{
+    	echo "Correo no Enviado";
+    	header('Location: informacion.html');
     }
 ?>
